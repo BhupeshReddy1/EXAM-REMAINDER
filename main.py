6036296b153b773602f948d4a18cd5a3b92fad96
Bhupesh <135623290+BhupeshReddy1@users.noreply.github.com>
2023-07-12 06:27:57 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import requests
 import os
 import smtplib
 import datetime as dt
 import random
 import pandas as pd
-from twilio.rest import Client
-from twilio.http.http_client import TwilioHttpClient
-
-account_sid="AC6acdcd1c63d8fb69516c916c2bb92cbb"
-auth_token="7f7d33098fa6a480f0ed46c3a3ece5cd"
-
+import numpy as np
 current= dt.datetime.now()
 current_tuple=(current.month, current.day)
+# year=current.year
+# print(year)
+# can print day of the week, where 0 means monday. 1 means tuesday and so on
+
+my_email=os.environ['from_mail']
+app_password="jdtkyjnhuikjthgr"
 
 data=pd.read_csv("exams.csv")
+
 p=data['date']
 r=data['month']
 date_array = p.to_numpy()
@@ -26,12 +27,24 @@ for i in range(len(date_array)):
         month_array[i] -= 1
 
 
+
+
+
+
 exam_dict={(data_row["month"], data_row["date"]): data_row for (index,data_row) in data.iterrows()}
+
+
+
+
+
 if current_tuple in exam_dict:
     exam_students=exam_dict[current_tuple]
+    # letter_r=random.randint(1,3)
     file_path="new_data.csv"
+#print(birthday_person)
     with open(file_path) as f:
         contents=f.read()
+        #checking if it is end of the month or not
         if exam_students['date']>23:
             exam_students["date"]=exam_students["date"]-23
             exam_students["month"]+=1
@@ -41,17 +54,12 @@ if current_tuple in exam_dict:
         contents = contents.replace("[Number]", str(exam_students["sem"]))
         contents = contents.replace("[Date]", str(exam_students["date"]))
         contents = contents.replace("[Month]", str(exam_students["month"]))
-
-        proxy_client=TwilioHttpClient()
-        proxy_client.session.proxies={'https': os.environ['https_proxy']}
-        client = Client(account_sid, auth_token)
-        message = client.messages \
-            .create(
-            body=f"hello,{contents}",
-            from_="+12705887289", # this is twilio phone number
-            to="+916304512130" # actual number to receive sms
-        )
-        print(message.status)
+        #print(contents)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=app_password)
+        connection.sendmail(from_addr=my_email,
+                                to_addrs=os.environ['to_mail'],
+                                msg=f"Subject:Hello\n\n{contents}")
 
 
-#use python anywhere to run it daily/automatically once per day.
